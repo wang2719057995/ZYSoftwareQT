d6b33939178bf44480ecc2e84b39ffa333987d6c
Pin Wang <2719057995@qq.com>
2025-09-07 21:32:33 +0800

    init

---
+++ untitled_en_US.ts
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="mainwindow.ui" line="14"/>
+        <location filename="build/Desktop_Qt_5_15_2_MSVC2019_32bit-Debug/untitled_autogen/include/ui_mainwindow.h" line="56"/>
+        <source>MainWindow</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="27"/>
+        <location filename="build/Desktop_Qt_5_15_2_MSVC2019_32bit-Debug/untitled_autogen/include/ui_mainwindow.h" line="57"/>
+        <source>Hello</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
