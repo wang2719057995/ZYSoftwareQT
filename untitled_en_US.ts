869bf70dffd31e4b8efd2ff17e3c75cb0678ea86
Pin Wang <2719057995@qq.com>
2025-09-12 19:38:08 +0800

    changed to QT5

--- untitled_en_US.ts
+++ untitled_en_US.ts
@@ -6,12 +6,14 @@
     <message>
         <location filename="mainwindow.ui" line="14"/>
         <location filename="build/Desktop_Qt_5_15_2_MSVC2019_32bit-Debug/untitled_autogen/include/ui_mainwindow.h" line="56"/>
+        <location filename="build/Desktop_Qt_6_9_2_MSVC2022_64bit-Debug/untitled_autogen/include/ui_mainwindow.h" line="56"/>
         <source>MainWindow</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="27"/>
         <location filename="build/Desktop_Qt_5_15_2_MSVC2019_32bit-Debug/untitled_autogen/include/ui_mainwindow.h" line="57"/>
+        <location filename="build/Desktop_Qt_6_9_2_MSVC2022_64bit-Debug/untitled_autogen/include/ui_mainwindow.h" line="57"/>
         <source>Hello</source>
         <translation type="unfinished"></translation>
     </message>
